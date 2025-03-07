f6c10b2c8782c3577ca0ee6dcb30dd7093110c46
yoonustehrani <yoonustehrani28@gmail.com>
2025-03-07 23:08:16 +0330

    docker compose network issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
             $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
-            $this->app->register(TelescopeServiceProvider::class);
+            // $this->app->register(TelescopeServiceProvider::class);
         }
     }
 
