c8342cca3308b5d894a28a42daef25842d997c12
yoonustehrani <yoonustehrani28@gmail.com>
2025-05-22 12:44:34 +0330

    united multiple migration files considering tours migration file

--- database/migrations/2025_01_07_122725_create_tours_table.php
+++ database/migrations/2025_01_07_122725_create_tours_table.php
@@ -20,7 +20,13 @@ return new class extends Migration
             $table->boolean('active')->default(true)->index();
             $table->foreignIdFor(Location::class, 'origin_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->smallInteger('number_of_nights')->unsigned()->nullable();
+            $table->char('airline_code', 3)->nullable();
+            $table->char('payment_type', 1);
+            $table->tinyText('image_src');
+            $table->tinyText('image_alt')->nullable();
             $table->timestamps();
+            $table->timestamp('published_at');
+            $table->json('meta')->nullable();
         });
     }
 
