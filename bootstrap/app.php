53e3ac69399eabc86effca3d9b807300e54555b1
yoonustehrani <yoonustehrani28@gmail.com>
2025-06-10 23:25:18 +0330

    trust proxies laravel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             '/telescope/*'
         ]);
+        $middleware->trustProxies(at: ['*']);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
