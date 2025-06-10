de5fc1099f1e7ac576f49d6936e3faeb98d3dadf
yoonustehrani <yoonustehrani28@gmail.com>
2025-06-10 23:28:56 +0330

    trustproxied

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             '/telescope/*'
         ]);
-        $middleware->trustProxies(at: ['*']);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
